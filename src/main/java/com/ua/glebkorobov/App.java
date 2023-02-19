6b574709074a6fdaee37f822bcabd1d71dfd6197
Gleb Korobov <gkorobov9@gmail.com>
2023-02-19 14:13:08 +0200

    connected jacoco and added one test

--- src/main/java/com/ua/glebkorobov/App.java
+++ src/main/java/com/ua/glebkorobov/App.java
@@ -4,10 +4,19 @@ package com.ua.glebkorobov;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+    }
+
+    public boolean isPalindrome(String inputString) {
+        if (inputString.length() == 0) {
+            return true;
+        } else {
+            char firstChar = inputString.charAt(0);
+            char lastChar = inputString.charAt(inputString.length() - 1);
+            String mid = inputString.substring(1, inputString.length() - 1);
+            return (firstChar == lastChar) && isPalindrome(mid);
+        }
     }
 }
--- src/test/java/com/ua/glebkorobov/AppTest.java
+++ src/test/java/com/ua/glebkorobov/AppTest.java
@@ -1,5 +1,6 @@
 package com.ua.glebkorobov;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,13 +14,18 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         assertTrue( true );
         assertTrue( true );
         assertTrue( true );
         assertTrue( true );
+    }
 
-
+    @Test
+    public void whenEmptyString_thenAccept() {
+        App palindromeTester = new App();
+        assertTrue(palindromeTester.isPalindrome(""));
+        assertTrue(palindromeTester.isPalindrome("noon"));
+        assertFalse(palindromeTester.isPalindrome("noan"));
     }
 }
