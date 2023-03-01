db14ae41ee0b0c7b92e0a066dd5bb83dd263a535
Gleb Korobov <gkorobov9@gmail.com>
2023-03-01 20:34:26 +0200

    Changed some information un logger info

--- src/main/java/com/ua/glebkorobov/App.java
+++ src/main/java/com/ua/glebkorobov/App.java
@@ -28,21 +28,30 @@ public class App {
     private static void checkFormat() {
         if (System.getProperty(FORMAT_OF_FIGURES) == null ||
                 System.getProperty(FORMAT_OF_FIGURES).equalsIgnoreCase("int")) {
+            logger.info("Format is int");
             makeMultiplicationTableInteger();
         }else if (System.getProperty(FORMAT_OF_FIGURES).equalsIgnoreCase("byte")) {
+            logger.info("Format is byte");
             makeMultiplicationTableByte();
         }else if (System.getProperty(FORMAT_OF_FIGURES).equalsIgnoreCase("long")) {
+            logger.info("Format is long");
             makeMultiplicationTableLong();
         }else if (System.getProperty(FORMAT_OF_FIGURES).equalsIgnoreCase("double")) {
+            logger.info("Format is double");
             makeMultiplicationTableDouble();
         }else if (System.getProperty(FORMAT_OF_FIGURES).equalsIgnoreCase("float")) {
+            logger.info("Format is float");
             makeMultiplicationTableFloat();
+        }else{
+            logger.warn("wrong format");
+            throw new RuntimeException("wrong format");
         }
     }
 
     private static void makeMultiplicationTableInteger() {
         GetProperty property = new GetProperty(FILE_NAME_PROPERTIES);
 
+        logger.info("Got min max and increment by format int");
         int min = Integer.parseInt(property.getValueFromProperty(MIN));
         int max = Integer.parseInt(property.getValueFromProperty(MAX));
         int increment = Integer.parseInt(property.getValueFromProperty(INCREMENT));
@@ -61,6 +70,7 @@ public class App {
     private static void makeMultiplicationTableByte(){
         GetProperty property = new GetProperty(FILE_NAME_PROPERTIES);
 
+        logger.info("Got min max and increment by format byte");
         byte min = Byte.parseByte(property.getValueFromProperty(MIN));
         byte max = Byte.parseByte(property.getValueFromProperty(MAX));
         byte increment = Byte.parseByte(property.getValueFromProperty(INCREMENT));
@@ -78,6 +88,7 @@ public class App {
     private static void makeMultiplicationTableLong(){
         GetProperty property = new GetProperty(FILE_NAME_PROPERTIES);
 
+        logger.info("Got min max and increment by format long");
         long min = Long.parseLong(property.getValueFromProperty(MIN));
         long max = Long.parseLong(property.getValueFromProperty(MAX));
         long increment = Long.parseLong(property.getValueFromProperty(INCREMENT));
@@ -95,6 +106,7 @@ public class App {
     private static void makeMultiplicationTableDouble(){
         GetProperty property = new GetProperty(FILE_NAME_PROPERTIES);
 
+        logger.info("Got min max and increment by format double");
         double min = Double.parseDouble(property.getValueFromProperty(MIN));
         double max = Double.parseDouble(property.getValueFromProperty(MAX));
         double increment = Double.parseDouble(property.getValueFromProperty(INCREMENT));
@@ -112,6 +124,7 @@ public class App {
     private static void makeMultiplicationTableFloat(){
         GetProperty property = new GetProperty(FILE_NAME_PROPERTIES);
 
+        logger.info("Got min max and increment by format float");
         float min = Float.parseFloat(property.getValueFromProperty(MIN));
         float max = Float.parseFloat(property.getValueFromProperty(MAX));
         float increment = Float.parseFloat(property.getValueFromProperty(INCREMENT));
